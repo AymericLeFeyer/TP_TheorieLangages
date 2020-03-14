a6338e6b059f2bb24ba3548d96c94fd6351dc93f
AymericLeFeyer <lefeyer.aymeric@gmail.com>
2020-03-14 17:59:54 +0100

    initial commit

---
+++ src/graphes/JGraph.java
@@ -0,0 +1,34 @@
+package graphes;
+
+import javax.swing.JFrame;
+
+import com.mxgraph.model.mxCell;
+import com.mxgraph.model.mxGeometry;
+import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.view.mxGraph;
+import java.util.regex.*;
+
+public class JGraph extends JFrame {
+
+    /** Pour éviter un warning venant du JFrame */
+    private static final long serialVersionUID = -8123406571694511514L;
+
+    public JGraph() {
+        super("JGrapghX tutoriel: Exemple 1");
+
+        mxGraph graph = new mxGraph();
+        Object parent = graph.getDefaultParent();
+
+        graph.getModel().beginUpdate();
+        try {
+            Object v1 = graph.insertVertex(parent, null, "Hello", 20, 20, 80, 30);
+            Object v2 = graph.insertVertex(parent, null, "World!", 240, 150, 80, 30);
+            graph.insertEdge(parent, null, "Edge", v1, v2);
+        } finally {
+            graph.getModel().endUpdate();
+        }
+
+        mxGraphComponent graphComponent = new mxGraphComponent(graph);
+        getContentPane().add(graphComponent);
+    }
+}
---
+++ src/graphes/Main.java
@@ -0,0 +1,15 @@
+package graphes;
+
+import javax.swing.*;
+
+public class Main {
+    /**
+     * @param args "arguments"
+     */
+    public static void main(String[] args) {
+        JGraph frame = new JGraph();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 320);
+        frame.setVisible(true);
+    }
+}
