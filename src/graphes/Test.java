b80715df3de95caf38fd0327fdd4141a0b923706
AymericLeFeyer <lefeyer.aymeric@gmail.com>
2020-04-06 13:06:04 +0200

    final commit

---
+++ src/graphes/Test.java
@@ -0,0 +1,14 @@
+package graphes;
+
+
+import java.util.Date;
+
+public class Test {
+    public static void main(String[] args) {
+        Date aujourdhui = new Date();
+        System.out.println(aujourdhui);
+        System.out.println(aujourdhui.getTime());
+
+    }
+
+}
